0ff845b59900fe7b0754f17f5802d17f61d6a87f
Christopher J. Wood <cjwood@us.ibm.com>
2023-06-15 12:25:39 -0400

    Add `scipy_alias` function (#13)
    Co-authored-by: Daniel Puzzuoli <dan.puzzuoli@gmail.com>

--- arraylias/default_alias/__init__.py
+++ arraylias/default_alias/__init__.py
@@ -5,6 +5,7 @@
 # Any modifications or derivative works of this code must retain this
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
-"""Numpy-syntax alias"""
+"""Default configured aliases"""
 
 from .numpy_alias import numpy_alias
+from .scipy_alias import scipy_alias

53c4a1d1b46ba207282544c37fe747e8fb8aa6f2
DanPuzzuoli <dan.puzzuoli@gmail.com>
2023-09-26 10:52:50 -0700

    incrememnting to version 0.2.0 post 0.1.0 release

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ author = 'Qiskit Development Team'
 
 
 # The short X.Y version.
-version = '0.1.0'
+version = '0.2.0'
 # The full version, including alpha/beta/rc tags.
-release = '0.1.0'
+release = '0.2.0'
 
 extensions = ['sphinx.ext.napoleon',
               'sphinx.ext.autodoc',
